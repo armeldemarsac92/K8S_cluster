aae93726a2e1105f4332a6774aefe47ea125d4f5
Armel De Marsac <armeldemarsac@gmail.com>
2025-04-14 17:40:48 +0200

    feat : vagrant config init

---
+++ vagrant/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#first we import the vagrant boxes
+BOXES_PATH="./boxes"
+
+if [ ! -d "$BOXES_PATH" ]; then
+    echo "Error: Boxes directory not found at ${BOXES_PATH}"
+    exit 1
+fi
+
+echo "Adding boxes from: ${BOXES_PATH}"
+
+# Add each box to Vagrant
+vagrant box add --name frontend_server "${BOXES_PATH}/machine_1.box"
+vagrant box add --name control_server "${BOXES_PATH}/machine_4.box"
+vagrant box add --name backend_server "${BOXES_PATH}/machine_2.box"
+vagrant box add --name database_server "${BOXES_PATH}/machine_3.box"
+
+echo "All boxes have been added successfully!"
+
+echo "Launching Vagrant configuration"
+
+vagrant up
+
+
